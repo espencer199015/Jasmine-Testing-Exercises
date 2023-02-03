a688f6b68b49c1d3d02bcece972e40fa4f23a993
Liz Erlandson <elizabeth.erlandson1@gmail.com>
2023-02-03 10:26:35 -0500

    updates to be pushed to git hub on testing cade page

--- calculator-test.js
+++ calculator-test.js
@@ -2,21 +2,29 @@
 it('should calculate the monthly rate correctly', function () {
   
   const values = {
-    amount: 10000,
-    years: 8,
-    rate: 5.8
+    amount: 100000,
+    years: 20,
+    rate: 4.5
   };
-expect(calculateMonthlyPayment(values)).toEqual('130.44');
+expect(calculateMonthlyPayment(values)).toEqual('632.65');
 });
 
 
 
 it("should return a result with 2 decimal places", function() {
   const values = {
-    amount: 10043,
-    years: 8,
-    rate: 5.8
+    amount: 100037,
+    years: 20,
+    rate: 4.5
   };
-  expect(calculateMonthlyPayment(values)).toEqual('131.00');
+  expect(calculateMonthlyPayment(values)).toEqual('632.88');
 });
 
+it("should handle high interest rates", function() {
+  const values = {
+    amount: 100000,
+    years: 40,
+    rate: 99
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('8250.00');
+});
